dacb463cb1e172cd340f86f03d1df7fabab90c57
Cesar Rodelin Junior <hadajunior5@gmail.com>
2023-06-25 00:54:24 -0400

    Minimal changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
-import NavBar from "@/components/NavBar/NavBar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+
+import NavBar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
